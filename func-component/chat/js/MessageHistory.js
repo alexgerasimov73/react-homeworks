16d1288abb5109086bdbbc6ce2b0a234254509bd
Alex Gerasimov <ag73@list.ru>
2018-07-17 23:52:00 +0300

    Добавлено решение

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const MessageHistory = props => {
+    if(!Array.isArray(props.list) || props.list.length == 0) {
+        return null;
+    }
+    const listForRendering = props.list.map(message => {
+        const CurMessage = message.type === 'response' ? Response : message.type === 'message' ? Message : Typing;
+        return (
+          <li key={message.id}>
+            <CurMessage from={message.from} message={message}/>
+          </li>
+        )    
+    })
+    return(
+        <ul>{listForRendering}</ul>
+    )
+}
--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,16 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars({count}) {
+  if (count < 1 || count > 5 || typeof(count) != "number") {
+    return null;
+  } 
+  const amountOfStars = [];
+  for (let i = 0; i < count; i++) {
+    amountOfStars.push(<Star />)
+  }
+  return (
+    <ul class="card-body-stars u-clearfix">
+      {amountOfStars}
+    </ul>    
+  )
 }
