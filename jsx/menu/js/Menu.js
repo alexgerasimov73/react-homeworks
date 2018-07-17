ee0b5609d19c1e5be89b080d5b8c6ade5dc012a4
Alex Gerasimov <ag73@list.ru>
2018-07-17 22:13:56 +0300

    Добавлено решение задач

---
+++ jsx/menu/js/Menu.js
@@ -0,0 +1,22 @@
+const Menu = ({items, opened}) => {
+  const {title, href} = items;
+  if (opened) {
+    return (
+      <div className="menu menu-open">
+        <div className="menu-toggle"><span></span></div>
+        <nav>
+          <ul>
+            {items.map((item, index) => 
+              <li key={index}><a href={item.href}>{item.title}</a></li>)}
+          </ul>
+        </nav>
+      </div>
+    )
+  } else {
+    return (
+      <div className="menu">
+        <div className="menu-toggle"><span></span></div>
+      </div>
+    )
+  }
+}
