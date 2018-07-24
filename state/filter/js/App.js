003295b68d9175c25fdf11e373c4cbc3ad974492
Alex Gerasimov <ag73@list.ru>
2018-07-24 23:56:23 +0300

    Добавлено решение

--- state/filter/js/App.js
+++ state/filter/js/App.js
@@ -1,11 +1,34 @@
 'use strict'
 
-const App = props => (
-  <div>
-    <Toolbar
-      filters={props.filters}
-      selected={'All'}
-      onSelectFilter={(filter) => console.log(filter)} />
-    <Portfolio projects={props.projects} />
-  </div>
-);
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      selected: "All"
+    };
+  }
+  
+  onSelectFilter(filter) {
+    this.setState({selected: filter});
+  }
+  
+  filterProjects() {
+    if(this.state.selected != "All") {
+      return this.props.projects.filter(project => project.category === this.state.selected);
+    }
+    return this.props.projects;
+  }
+  
+  render() {
+    return (
+      <div>
+        <Toolbar
+          filters={this.props.filters}
+          selected={this.state.selected}
+          onSelectFilter={this.onSelectFilter.bind(this)}
+        />
+        <Portfolio projects={this.filterProjects()} />
+      </div>
+    );
+  }
+}
