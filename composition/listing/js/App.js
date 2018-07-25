6c593808222725a7b74bcc2587c6339e69b5177c
Alex Gerasimov <ag73@list.ru>
2018-07-25 23:34:44 +0300

    Добавлено решение

--- composition/listing/js/App.js
+++ composition/listing/js/App.js
@@ -1,16 +1,23 @@
 'use strict';
 
 const App = ({items}) => (
-  <main>
-    {items.map(item => {
-      switch(item.type) {
-        case 'unisex':
-          return <Item color="black" item={item} />;
-        case 'male':
-          return <Item color="blue" item={item} />;
-        case 'female':
-          return <Item color="orange" item={item} />;
-      }
-    })}
-  </main>
+  <Items items={items} />
 );
+
+const Items = ({items}) => {
+    const showItems = items.map(item => {
+        switch(item.type) {
+          case 'unisex':
+            return <Item color="black" item={item} />;
+          case 'male':
+            return <Item color="blue" item={item} />;
+          case 'female':
+            return <Item color="orange" item={item} />;
+        }
+    });
+    return (
+        <main>
+            {showItems}
+        </main>
+    )
+}
